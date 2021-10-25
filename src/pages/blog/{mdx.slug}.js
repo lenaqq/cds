85a74bd07fd39cd764afee93dd951cc605969f39
Lena Qian <lenaqian@Lenas-iMac.local>
2021-10-25 15:53:35 +1100

    Dynamic images wiht Gatsby image plugin

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,14 +1,26 @@
-import * as React from "react";
-import Layout from "../../Components/layout";
+import * as React from "react"
 import { graphql } from "gatsby"
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { GatsbyImage, getImage  } from "gatsby-plugin-image"
+import Layout from "../../Components/layout"
 
 const BlogPostPage = ( { data}) => {
-    console.log(data);
+    const image = getImage(data.mdx.frontmatter.hero_image);
+    console.log(image);
 
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
+            <p>
+                Photo Credit:{" "}
+                <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -17,15 +29,26 @@ const BlogPostPage = ( { data}) => {
 };
 
 export const query = graphql`
-query ($id:String) {
+query MyQuery($id:String) {
   mdx(id: {eq: $id}) {
-    id
     frontmatter {
-      date
-      title
-    }
-    body
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        date
+        title
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(
+                aspectRatio: 1.5
+                transformOptions: {cropFocus: ATTENTION}
+            )
+          }
+        }
+      }
+      body
   }
 }
 `
+
 export default BlogPostPage;
